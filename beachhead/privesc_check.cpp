90e9fbf1b089dd832d7ab735f74f6469db3596ad
Joshua Tackie <140110118+TitanStriker@users.noreply.github.com>
2026-04-20 17:45:24 -0400

    Replace linux/dirent.h with dirent.h

--- beachhead/privesc_check.cpp
+++ beachhead/privesc_check.cpp
@@ -16,7 +16,7 @@
 #include <sys/syscall.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <linux/dirent.h>
+#include <dirent.h>
 #include <cstring>
 
 // ============================================================================
