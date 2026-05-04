d0737c22707fae975bf86c0207d353e1270ff0c9
deutscherSchaferhund44 <bt.wei1234@gmail.com>
2026-05-03 21:28:49 -0400

    Draft of multi-connection server.py, requires debugging

--- server.py
+++ server.py
@@ -4,22 +4,49 @@ import ssl
 import random
 import threading
 
-host = '0.0.0.0'  # Open to anyone on the same wifi
+host = '0.0.0.0' # Open to anyone on the same wifi
 port = 8888
 
 types = ['HELO', 'EXIT', 'READ', 'RITE', 'CMD', 'ERR', 'RECON']
 
-def parseAndSendInput(s: ssl.SSLSocket, user_input):
+lock = threading.Lock()
+addresses = [ ]
+
+def parseAndSendInput(user_input):
     user_input = user_input.split(' ')
-    type = user_input[0]
-    id = str(random.randint(0, 10 ** 9))
-    data = " ".join(user_input[1:])
-    assert type in types, f"unknown type {type} of length {len(type)}"
-    if type in ['READ', 'RITE', 'CMD']:
-        assert(data != '')
-    else:
-        assert(data == '')
-    s.send(("  ".join([type, id, data])).encode())
+
+    try:
+        type = user_input[0]
+
+        # handle server exclusive commands
+        if type is 'listconns':
+            print("List of connections:\n")
+            idx = 0
+            for e in addresses:
+                print(f"\t[{idx}]: {addr}", idx, addresses[0])
+            return
+
+        if type is 'help':
+            print("Usage: [server command] [connection id] [params...]"
+                  "Server local commands: \'help\', \'listconns\' "
+                  "Client affecting commands: \'HELO\', \'EXIT\', \'READ\', \'RITE\', \'CMD\', \'ERR\', \'RECON\'")
+            return
+
+        id = str(random.randint(0, 10 ** 9))
+        addressIdx = user_input[1]
+        connection = addresses[int(addressIdx)][1]
+
+        # handle server -> client commands
+        data = " ".join(user_input[2:])
+        assert type in types, f"unknown type {type} of length {len(type)}"
+        if type in ['READ', 'RITE', 'CMD']:
+            assert(data != '')
+        else:
+            assert(data == '')
+
+        connection.send(("  ".join([type, id, data])).encode())
+    except:
+        print("Something went wrong. Type 'help' for details on usage.")
 
 def receiveMessage(c: ssl.SSLSocket):
     while True:
@@ -48,11 +75,14 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as raw_socket:
     print(f"Listening on {host}:{port} (TLS)")
 
     with ssl_context.wrap_socket(raw_socket, server_side=True) as s:
-        c, a = s.accept()
 
-        # On connecting, use threading to both send and receive messages appropriately
-        threading.Thread(target=receiveMessage, args=(c,), daemon=True).start()
-        with c:
-            print(f"Connected to {a}")
-            while True:
-                parseAndSendInput(c, input('> '))
+        # While true, accept new connections and register them 
+        threading.Thread(target=parseAndSendInput, args=(input('> ')), daemon=True).start()
+        while True:
+            c, a = s.accept()
+
+            with c:
+                threading.Thread(target=receiveMessage, args=(c,), daemon=True).start()
+                print(f"Connected to {a}")
+                with lock:
+                    addresses.append((a, c))
